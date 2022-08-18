c2df4a60557e32cdd7078ab8c8d8bac1d4c79a5f
Corinne Kinzy <corinne.kinzy@ambition.com>
2022-08-18 12:31:33 -0400

    add test

--- data_schema/tests/model_tests.py
+++ data_schema/tests/model_tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
+from django_dynamic_fixture import G
 
-from data_schema.models import FieldSchema
+from data_schema.models import FieldSchema, DataSchema
 
 
 class FieldSchemaTest(TestCase):
@@ -9,3 +10,17 @@ class FieldSchemaTest(TestCase):
         field = FieldSchema(field_key='key', display_name='Field', id=10)
 
         self.assertEqual(str(field), u'10 - key - Field')
+
+    def test_display_name_no_character_limit(self):
+        data_schema = G(DataSchema)
+        field_schema = FieldSchema(
+            data_schema=data_schema,
+            field_key='testing_id',
+            display_name='Testing this is longer than 64 characters and we are no longer failing')
+
+        field_schema.save()
+
+        self.assertEqual(
+            field_schema.display_name,
+            'Testing this is longer than 64 characters and we are no longer failing'
+        )
